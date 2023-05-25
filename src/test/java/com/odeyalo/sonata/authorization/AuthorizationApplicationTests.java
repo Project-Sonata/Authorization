15dce612bb19d638047a1a5f9137f6852e600543
odeyalo <theprogrammerinfuture@gmail.com>
2023-05-25 20:03:04 +0300

    Tests for application to check context bootstrap

---
+++ src/test/java/com/odeyalo/sonata/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
