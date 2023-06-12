8ea5752c2670c2b332103fde0016fc09e99bea57
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-12 20:40:32 +0300

    SonataAuthenticationProvider implementation that provide authentication using  ReactiveAuthenticationClient

---
+++ src/main/java/com/odeyalo/sonata/authorization/service/authentication/RemoteSonataAuthenticationProvider.java
@@ -0,0 +1,32 @@
+package com.odeyalo.sonata.authorization.service.authentication;
+
+import com.odeyalo.sonata.common.authentication.dto.LoginCredentials;
+import com.odeyalo.sonata.common.authentication.exception.LoginAuthenticationFailedException;
+import com.odeyalo.sonata.common.authentication.reactive.ReactiveAuthenticationClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+
+/**
+ * SonataAuthenticationProvider implementation that provide authentication using
+ * the {@link ReactiveAuthenticationClient}
+ */
+@Component
+public class RemoteSonataAuthenticationProvider implements SonataAuthenticationProvider {
+    private final ReactiveAuthenticationClient reactiveAuthenticationClient;
+
+    public RemoteSonataAuthenticationProvider(ReactiveAuthenticationClient reactiveAuthenticationClient) {
+        this.reactiveAuthenticationClient = reactiveAuthenticationClient;
+    }
+
+    @Override
+    public Mono<SonataAuthentication> obtainAuthentication(String username, String password) throws LoginAuthenticationFailedException {
+        return reactiveAuthenticationClient.login(LoginCredentials.of(username, password))
+                .flatMap(HttpEntity::getBody)
+                .map(body -> SonataAuthentication.of(body.getUserInfo(), password, body.getUserInfo(), Set.of(new SimpleGrantedAuthority("USER"))))
+                .onErrorResume(ex -> true, Mono::error);
+    }
+}
