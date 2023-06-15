4a18f770660d74a91efcbf11d4fefb31922ba92d
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-15 14:29:45 +0300

    Added equals

--- src/main/java/com/odeyalo/sonata/authorization/support/scope/AbstractScope.kt
+++ src/main/java/com/odeyalo/sonata/authorization/support/scope/AbstractScope.kt
@@ -14,11 +14,24 @@ abstract class AbstractScope(
         return description
     }
 
+
     override fun supportsRole(roleName: String): Boolean {
         return supportedRoles.contains(roleName)
     }
 
     override fun toString(): String {
         return "AbstractScope(name='$name', description='$description', supportedRoles=$supportedRoles)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AbstractScope
+
+        if (name != other.name) return false
+        if (description != other.description) return false
+        if (supportedRoles != other.supportedRoles) return false
+        return true
     }
 }
