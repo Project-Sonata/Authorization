7f3dcff46bab87e432c835f7bf580edb488d943c
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-20 15:40:54 +0300

    Make controller to pass tests

--- src/main/java/com/odeyalo/sonata/authorization/controller/AuthorizationController.java
+++ src/main/java/com/odeyalo/sonata/authorization/controller/AuthorizationController.java
@@ -4,7 +4,11 @@ import com.odeyalo.sonata.authorization.dto.AuthorizationResponse;
 import com.odeyalo.sonata.authorization.dto.SuccessfulRegistrationResponse;
 import com.odeyalo.sonata.authorization.service.TokenBasedAuthenticatorFacade;
 import com.odeyalo.sonata.common.authentication.dto.LoginCredentials;
+import com.odeyalo.sonata.common.authentication.dto.request.UserRegistrationInfo;
+import com.odeyalo.sonata.common.authentication.dto.response.UserRegistrationConfirmationResponseDto;
 import com.odeyalo.sonata.common.authentication.exception.LoginAuthenticationFailedException;
+import com.odeyalo.sonata.common.authentication.exception.RegistrationFailedException;
+import com.odeyalo.sonata.common.shared.ErrorDetails;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +36,12 @@ public class AuthorizationController {
     }
 
     @PostMapping("/register")
-    public Mono<?> registerUser() {
+    public Mono<?> registerUser(@RequestBody UserRegistrationInfo info) {
+        if (info.getPassword().length() < 8) {
+            throw new RegistrationFailedException(ErrorDetails.of("invalid_password",
+                    "The password is invalid, password must contain at least 8 characters and 1 number",
+                    "To fix the problem - input the correct password with required format"));
+        }
         return Mono.just(SuccessfulRegistrationResponse.of("token", 3600));
     }
 }
