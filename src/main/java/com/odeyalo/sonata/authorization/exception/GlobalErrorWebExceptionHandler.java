f4eaf9a2b9634ee0b013f8216efb62dd92b94b05
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-20 15:42:05 +0300

    Added RegistrationFailedException handler

--- src/main/java/com/odeyalo/sonata/authorization/exception/GlobalErrorWebExceptionHandler.java
+++ src/main/java/com/odeyalo/sonata/authorization/exception/GlobalErrorWebExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.odeyalo.sonata.authorization.exception;
 
 import com.odeyalo.sonata.common.authentication.exception.InvalidCredentialsException;
+import com.odeyalo.sonata.common.authentication.exception.RegistrationFailedException;
 import com.odeyalo.sonata.common.shared.ErrorDetails;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalErrorWebExceptionHandler {
 
     @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<ErrorDetails> errorHandle(InvalidCredentialsException e) {
+    public ResponseEntity<ErrorDetails> handleInvalidCredentialsException(InvalidCredentialsException e) {
+        return ResponseEntity.badRequest().body(e.getErrorDetails());
+    }
+
+    @ExceptionHandler(RegistrationFailedException.class)
+    public ResponseEntity<ErrorDetails> handleRegistrationFailedException(RegistrationFailedException e) {
         return ResponseEntity.badRequest().body(e.getErrorDetails());
     }
 }
