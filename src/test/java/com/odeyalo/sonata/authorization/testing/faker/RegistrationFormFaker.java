c2e6ecb04e6d9da5cc9fc7cf88e3754f5fcbf010
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-27 20:49:10 +0300

    Added build method

--- src/test/java/com/odeyalo/sonata/authorization/testing/faker/RegistrationFormFaker.java
+++ src/test/java/com/odeyalo/sonata/authorization/testing/faker/RegistrationFormFaker.java
@@ -79,6 +79,9 @@ public class RegistrationFormFaker {
         return overrideEnableNotification(true);
     }
 
+    public RegistrationForm build() {
+        return creator.create();
+    }
 
     @Getter
     @Setter
@@ -99,6 +102,5 @@ public class RegistrationFormFaker {
         public static final String MALE = "MALE";
         public static final String FEMALE = "FEMALE";
         public static final String NONE = "NONE";
-
     }
 }
