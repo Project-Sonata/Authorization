5892e46e4a882d1506f9a11e6354130a639fae1b
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-29 20:44:18 +0300

    Added import for SuiteReactiveAutoConfiguration

--- src/main/java/com/odeyalo/sonata/authorization/AuthorizationApplication.java
+++ src/main/java/com/odeyalo/sonata/authorization/AuthorizationApplication.java
@@ -1,13 +1,16 @@
 package com.odeyalo.sonata.authorization;
 
+import com.odeyalo.sonata.suite.reactive.config.SuiteReactiveAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @EnableReactiveFeignClients(basePackages = "com.odeyalo.sonata.suite.reactive")
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(SuiteReactiveAutoConfiguration.class)
 public class AuthorizationApplication {
 
     public static void main(String[] args) {
