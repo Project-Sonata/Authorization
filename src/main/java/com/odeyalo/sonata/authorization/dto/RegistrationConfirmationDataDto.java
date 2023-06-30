61bba5e32621287f811caaa0d3e4dff79df39ae9
odeyalo <theprogrammerinfuture@gmail.com>
2023-06-30 16:45:47 +0300

    Added JsonProperty

--- src/main/java/com/odeyalo/sonata/authorization/dto/RegistrationConfirmationDataDto.java
+++ src/main/java/com/odeyalo/sonata/authorization/dto/RegistrationConfirmationDataDto.java
@@ -1,5 +1,6 @@
 package com.odeyalo.sonata.authorization.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -9,5 +10,6 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RegistrationConfirmationDataDto {
+    @JsonProperty("data")
     String data;
 }
