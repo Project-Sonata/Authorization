23db096817b746f4ef5ac37ab8aaff6beafb2a8b
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-19 21:12:00 +0200

    Represent Oauth2AccessToken saved in any kind of database

---
+++ src/main/java/com/odeyalo/sonata/authorization/entity/Oauth2AccessTokenEntity.java
@@ -0,0 +1,27 @@
+package com.odeyalo.sonata.authorization.entity;
+
+import com.odeyalo.sonata.authorization.service.token.oauth2.ScopeContainer;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+/**
+ * Represent Oauth2AccessToken saved in any kind of database
+ */
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Oauth2AccessTokenEntity {
+    Long id;
+    String tokenValue;
+    long issuedAtSeconds;
+    long expiresInSeconds;
+    ScopeContainer scopes;
+
+    public boolean isValid() {
+        return expiresInSeconds < Instant.now().getEpochSecond();
+    }
+}
